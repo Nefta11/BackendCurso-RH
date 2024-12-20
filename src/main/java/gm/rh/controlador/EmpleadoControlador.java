8739fc7e8f0d0c4f4f669cd223da4bc4a6636d5f
Nefta11 <neftaliarturohernandez@gmail.com>
2024-12-20 12:43:30 -0600

    Agregar controlador EmpleadoControlador con configuración básica y servicio inyectado

---
+++ src/main/java/gm/rh/controlador/EmpleadoControlador.java
@@ -0,0 +1,24 @@
+package gm.rh.controlador;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import gm.rh.servicio.IEmpleadoServicio;
+
+@RestController
+@RequestMapping("rh.app") // http://localhost:8080/rh.app
+@CrossOrigin(value = "hhtp://localhost:3000")
+
+public class EmpleadoControlador {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(EmpleadoControlador.class);
+
+    @Autowired
+    private IEmpleadoServicio empleadoServicio;
+
+}
