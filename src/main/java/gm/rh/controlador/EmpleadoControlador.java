c6ebe6c288618e8810147d5866cec8262694d74e
Nefta11 <neftaliarturohernandez@gmail.com>
2024-12-20 17:40:38 -0600

    Agregar método para obtener un empleado por ID y crear excepción personalizada para recursos no encontrados

--- src/main/java/gm/rh/controlador/EmpleadoControlador.java
+++ src/main/java/gm/rh/controlador/EmpleadoControlador.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,5 +40,13 @@ public class EmpleadoControlador {
         logger.info("Empleado a agregar: " + empleado);
         return empleadoServicio.guardarEmpleado(empleado);
     }
+
+    @GetMapping("/empleados/{id}")
+    public ResponseEntity<Empleado>
+    obtenerEmpleadoPorId(@PathVariable Integer id){
+        Empleado empleado = empleadoServicio.buscarEmpleadoPorId(id);
+        if 
+    }
+    
 
 }
